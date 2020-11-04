ec34708cb69677abf63b7a6143906e0091eeae01
Radu Dumbraveanu <vundicind@gmail.com>
2020-11-04 17:02:07 +0200

    New modifications for attribute converter tests

--- attribute-converter-integration-testing-shaded/application/src/main/java/com/example/multimodule/application/DemoApplication.java
+++ attribute-converter-integration-testing-shaded/application/src/main/java/com/example/multimodule/application/DemoApplication.java
@@ -1,11 +1,18 @@
 package com.example.multimodule.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = "com.example.multimodule")
+@EntityScan("com.example.multimodule.library")
+@EnableJpaRepositories(basePackages = {
+		"com.example.multimodule.library"
+})
 @RestController
 public class DemoApplication {
 
@@ -15,6 +22,9 @@ public class DemoApplication {
 //		this.myService = myService;
 //	}
 //
+	@Autowired
+	private EmployeeFacade employeeFacade;
+
 	@GetMapping("/")
 	public String home() {
 //		return myService.message();
