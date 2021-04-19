e8aa38cfbe8aa2349d6f76a9626407a380dc182e
Radu Dumbraveanu <vundicind@gmail.com>
2021-04-19 14:12:23 +0300

    Remove attribute-converter-integration-testing-shaded

--- attribute-converter-integration-testing-shaded/library/src/main/java/com/example/multimodule/library/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.multimodule.library;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-}
